512e91a6b7885b553bdf8ec0243817494ba540c9
Renato Pacheco <renato.pacheco@adm.unip.br>
2020-01-06 19:13:31 -0300

    Add test to TextHandle.ProperName

---
+++ test/TextHandleTest/ProperNameTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BitHelp.Core.HandleData.Test.TextHandleTest
+{
+    public class ProperNameTest
+    {
+        [Fact]
+        public void Using_in_names_with_all_letters_in_upercase()
+        {
+            string name = "RENATO BEVILACQUA PACHECO";
+            Assert.Equal("Renato Bevilacqua Pacheco", TextHandle.ProperName(name));            
+        }
+
+        [Fact]
+        public void Using_in_names_with_all_letters_in_lowercase()
+        {
+            string name = "renato bevilacqua pacheco";
+            Assert.Equal("Renato Bevilacqua Pacheco", TextHandle.ProperName(name));
+        }
+
+        [Fact]
+        public void Using_in_names_with_special_characters_and_all_letters_in_lowercase()
+        {
+            string name = "andré magalhães gonçalves";
+            Assert.Equal("André Magalhães Gonçalves", TextHandle.ProperName(name));
+        }
+    }
+}
